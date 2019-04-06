b45f2f9d45cb36e4ae73469a08dfb8b7bd202970
Zarina Mazieva <zmazieva78@gmail.com>
2019-04-06 08:18:30 -0700

    heroku

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const path = require("path");
 
 if(process.env.NODE_ENV === 'production'){
   //set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client/public'));
 }
 
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.get('/',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
 });
 
 // Bodyparser middleware
