a05e142391634a2f11054d9263efb25db6c4afe5
Zarina Mazieva <zmazieva78@gmail.com>
2019-04-06 08:28:34 -0700

    local

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/public'));
 }
 
-app.get('/',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-});
 
 // Bodyparser middleware
 app.use(
@@ -38,5 +35,10 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+
+app.get('/',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
